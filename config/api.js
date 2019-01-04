975eb573fdd6be11d9ad259500fce440d81f3e1f
xianengqi <920986617@qq.com>
2019-01-04 22:48:53 +0800

    update menu

--- config/api.js
+++ config/api.js
@@ -7,4 +7,8 @@ const ApiRootUrl = 'http://127.0.0.1:3008'
 module.exports = {
   IndexUrl: ApiRootUrl + '/home',  // 首页数据接口
   AuthLoginByWeixin: ApiRootUrl + '/users/wxLogin', // 微信登录接口
+  CatalogList: ApiRootUrl + '/category/indexAction', // 分类目录全部分类数据接口
+  CatalogCurrent: ApiRootUrl + '/category/currentAction', // 分类目录当前分类数据接口
+  Catalogory: ApiRootUrl + '/category/categoryNav',
+  GoodsCount: ApiRootUrl + '/goods/goodsList', // 统计商品总数
 }
---
+++ pages/catalog/index.js
@@ -0,0 +1,99 @@
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+
+Page({
+  data:{
+    navList: [],
+    categoryList: [],
+    currentCategory: {},
+    goodsCount: 0,
+    scrollLeft: 0,
+    scrollTop: 0,
+    scrollHeight: 0,
+    currentOne: []
+  },
+
+  getCatalog: function () {
+    let that = this;
+    wx.showLoading({
+      title: '加载中...',
+    });
+    util.request(api.CatalogList).then(function (res) {
+      console.log('分类',res);
+      const data = res.results;
+      that.setData({
+        navList: data.categoryList.rows,
+        currentCategory: data.currentCategory.rows,
+      });
+      wx.hideLoading();
+    });
+    // util.request(api.GoodsCount).then(function (res) {
+    //   console.log('good', res);
+    //   const data = res.results;
+    //   that.setData({
+    //     goodsCount: data.goodsCount.rows
+    //   });
+    // });
+  },
+
+  getCurrentCategory: function (id) {
+    let that = this;
+    util.request(api.CatalogCurrent, {id: id})
+      .then(function (res) {
+        console.log('idCurrent', res);
+        const data = res.results;
+        that.setData({
+          currentCategory: data.subList.rows,
+          currentOne: data.currentOne.rows,
+        })
+      })
+  },
+  switchCate: function (event) {
+    console.log(event);
+    const that = this;
+    const currentTarget = event.currentTarget;
+
+    // if (this.data.currentCategory.id == event.currentCategory.dataset.id) {
+    //   return false;
+    // }
+    this.getCurrentCategory(event.currentTarget.dataset.id);
+  },
+  /* --------生命周期函数--------- */
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+    this.getCatalog();
+    // this.getCurrentCategory();
+  },
+  onReady:function(){
+    // 生命周期函数--监听页面初次渲染完成
+    
+  },
+  onShow:function(){
+    // 生命周期函数--监听页面显示
+    
+  },
+  onHide:function(){
+    // 生命周期函数--监听页面隐藏
+    
+  },
+  onUnload:function(){
+    // 生命周期函数--监听页面卸载
+    
+  },
+  onPullDownRefresh: function() {
+    // 页面相关事件处理函数--监听用户下拉动作
+    
+  },
+  onReachBottom: function() {
+    // 页面上拉触底事件的处理函数
+    
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      // title: 'title', // 分享标题
+      // desc: 'desc', // 分享描述
+      // path: 'path' // 分享路径
+    }
+  }
+})
